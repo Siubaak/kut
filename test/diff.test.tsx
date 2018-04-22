60bff1aa16bf3fb55b1aca1cc2f2f4e68fe051ba
Jimkwan <jimkwan.yau@gmail.com>
2018-04-22 17:17:21 +0800

    [Update] Add tests for diff (issue #3)

---
+++ test/diff.test.tsx
@@ -0,0 +1,110 @@
+import React from '../src/kut'
+import { KutElement } from '../src/element'
+import { diff, patch } from '../src/diff'
+import { DOMInstance } from '../src/instance'
+
+describe('test/render.test.js', () => {
+  function getListEleArr(list: number[]): KutElement[] {
+    return list.map(i => <div key={i}>{i}</div>)
+  }
+  function getListInstArr(list: KutElement[]): DOMInstance[] {
+    return list.map((ele, index) => {
+      const inst = new DOMInstance(ele)
+      inst.index = index
+      return inst
+    })
+  }
+  const oldList = getListEleArr([1, 2, 3, 4, 5])
+
+  it('should diff out only one forward move patch', () => {
+    const instList = getListInstArr(oldList)
+    const inst = instList[0]
+    const patches = diff(instList, getListEleArr([2, 3, 4, 5, 1]))
+
+    expect(patches.dir).toBe('forward')
+    expect(patches.ops.length).toBe(1)
+
+    const patch = patches.ops[0]
+
+    expect(patch.type).toBe('move')
+    expect(patch.inst).toBe(inst)
+    expect(patch.index).toBe(instList.length - 1)
+  })
+
+  it('should diff out only one backward move patch', () => {
+    const instList = getListInstArr(oldList)
+    const inst = instList[instList.length - 1]
+    const patches = diff(instList, getListEleArr([5, 1, 2, 3, 4]))
+    
+    expect(patches.dir).toBe('backward')
+    expect(patches.ops.length).toBe(1)
+
+    const patch = patches.ops[0]
+
+    expect(patch.type).toBe('move')
+    expect(patch.inst).toBe(inst)
+    expect(patch.index).toBe(0)
+  })
+
+  it('should diff out only one forward remove patch', () => {
+    const instList = getListInstArr(oldList)
+    const inst = instList[2]
+    const patches = diff(instList, getListEleArr([1, 2, 4, 5]))
+    
+    expect(patches.dir).toBe('backward')
+    expect(patches.ops.length).toBe(1)
+
+    const patch = patches.ops[0]
+
+    expect(patch.type).toBe('remove')
+    expect(patch.inst).toBe(inst)
+    expect(patch.index).toBeUndefined()
+  })
+
+  it('should diff out only one backward insert patch', () => {
+    const instList = getListInstArr(oldList)
+    const patches = diff(instList, getListEleArr([1, 2, 3, 4, 5, 6]))
+
+    expect(patches.dir).toBe('backward')
+    expect(patches.ops.length).toBe(1)
+
+    const patch = patches.ops[0]
+
+    expect(patch.type).toBe('insert')
+    expect(patch.inst.key).toBe('k_6')
+    expect(patch.index).toBe(5)
+  })
+
+  it('should diff and patch normally', () => {
+    const instList = getListInstArr(oldList)
+    let markup = ''
+    instList.forEach(inst => {
+      markup += inst.mount(inst.key)
+    })
+    document.body.dataset.kutid = 'kut'
+    document.body.innerHTML = markup
+    const patches = diff(instList, getListEleArr([5, 1, 2, 4, 3]))
+
+    expect(patches.dir).toBe('backward')
+    expect(patches.ops.length).toBe(2)
+    expect(patches.ops[0].type).toBe('move')
+    expect(patches.ops[0].inst.key).toBe('k_4')
+    expect(patches.ops[0].index).toBe(2)
+    expect(patches.ops[1].type).toBe('move')
+    expect(patches.ops[1].inst.key).toBe('k_5')
+    expect(patches.ops[1].index).toBe(0)
+
+    patch('kut', patches)
+    const childNodes = document.body.childNodes
+
+    expect(childNodes.length).toBe(5)
+    expect((childNodes[0] as any).dataset.kutid).toBe('k_5')
+    expect((childNodes[1] as any).dataset.kutid).toBe('k_1')
+    expect((childNodes[2] as any).dataset.kutid).toBe('k_2')
+    expect((childNodes[3] as any).dataset.kutid).toBe('k_4')
+    expect((childNodes[4] as any).dataset.kutid).toBe('k_3')
+
+    delete document.body.dataset.kutid
+    document.body.innerHTML = null
+  })
+})
